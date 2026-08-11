077405bda63d676501fe244e27f5dbb27c08f980
Antonin Goslin <antonin.goslin62@gmail.com>
2026-08-11 11:27:02 +0200

    v1 of test to debug constructors

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -1,8 +1,10 @@
 package fr.inria.verveine.extractor.java;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.lang.invoke.TypeDescriptor;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -29,5 +31,33 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 		parser.configure(sources);
 		parser.parse();
 		parser.exportModel(DEFAULT_OUTPUT_FILE);
+	}
+	
+	/*
+	 * These tests are here to check a bug of duplication happening on the entity typing
+	 */
+	@Test
+	public void testStubConstructorHasExactlyOneVoidTyping() {
+		parse(new String[] { "src/test/resources/entity_typing/MarshalledObject.java" });
+		
+		int voidTypingCount = 0;
+		
+		//declared type is void and typed entity is MarshalInputStream
+		for(EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			if(typing.getDeclaredType() != null && "void".equals(typing.getDeclaredType().getName())) {
+				TTypedEntity typedEntity = typing.getTypedEntity();
+				
+				// bug on MarshalInputStream
+				if(typedEntity instanceof NamedEntity) {
+					NamedEntity namedEntity = (NamedEntity) typedEntity;
+					if ("MarshalInputStream".equals(namedEntity.getName())) {
+						voidTypingCount++;
+					}
+				}
+			}
+		}
+		
+		//we should have only 1 void for MarshalInputStream
+		assertEquals(1, voidTypingCount);
 	}
 }
